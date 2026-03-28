f45e1ab4573d76479b9116d73c0ec2f2429cbb0b
Javier Daza <javier.daza@ucb.edu.bo>
2026-03-28 09:12:07 -0400

    Crear el componente menu para la vista cliente

---
+++ src/app/components/menu/menu.ts
@@ -0,0 +1,19 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductoService } from '../../services/producto.service';
+import { Producto } from '../../models/producto.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu.html',
+})
+export class MenuComponent implements OnInit {
+  private svc = inject(ProductoService);
+  private router = inject(Router);
+
+  
+  
+}
