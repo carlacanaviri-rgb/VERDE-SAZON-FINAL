2bc9120ea188c6be12fa94f27ab1268b0b2988f9
Libia71421 <baka65311131@gmail.com>
2026-04-09 21:28:41 -0400

    chore: remove legacy NestJS backend fast-food-api

--- fast-food-api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductosModule } from './productos/productos.module';
-import { FirebaseModule } from './firebase/firebase.module';
-import { LoggerModule } from './logger/logger.module';
-
-@Module({
-  imports: [FirebaseModule, ProductosModule, LoggerModule],
-})
-export class AppModule {}
