7a10c9962d8877f62cd0630dd6de1611f4b18a95
AdrianaC23MX <canaviri.alvarado.carla@gmail.com>
2026-04-14 17:16:36 -0400

    configuracion de Cores para backend

--- fast-food-app/src/environments/environment.development.ts
+++ fast-food-app/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environmentDevelopment = {
   production: false,
-  apiBaseUrl: 'https://verde-sazon-final-production.up.railway.app',
+  apiBaseUrl: 'http://localhost:3000',
   firebaseConfig: {
     apiKey: "AIzaSyCJl2KhPsfklxxRTaT9e20lb0N9V1OYfcw",
     authDomain: "verdesazon-92639.firebaseapp.com",
